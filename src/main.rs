570fcfb8fd5b3efc2f467ee40a3728696e8fb923
Franklin Van Nes <franklin.vannes@gmail.com>
2017-12-24 23:17:09 -0500

    Fully working and featured

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 extern crate time;
 
 use std::io;
+use std::io::Write;
 use std::thread;
 use time::Duration;
 
-const DEFAULT_TASK_MINS: u32 = 25;
-const DEFAULT_BREAK_MINS: u32 = 5;
+const DEFAULT_TASK_MINS: i64  = 25;
+const DEFAULT_BREAK_MINS: i64 = 5;
 
 fn main() {
     println!("Starting Pomodoro...\n");
 
-    println!("What is your next task?");
+    println!("What is your next task? ");
 
     let mut task_name = String::new();
 
@@ -19,8 +20,8 @@ fn main() {
     io::stdin().read_line(&mut task_name)
         .expect("Are you trying to break this thing? This is why you can't have nice things.");
 
-    let mut task_minutes  = 25;
-    let mut break_minutes = 5;
+    let task_minutes;
+    let break_minutes;
 
     loop {
         println!("How long will this take? (minutes) [default = 25]: ");
@@ -33,7 +34,7 @@ fn main() {
 
        task_minutes = match task_min_input.trim().parse() {
             Ok(mins) => mins,
-            Err(_)   => continue,
+            Err(_)   => DEFAULT_TASK_MINS,
         };
 
         println!("How long will the following break be? (minutes) [default = 5]: ");
@@ -44,7 +45,7 @@ fn main() {
 
         break_minutes = match break_min_input.trim().parse() {
             Ok(mins)    => mins,
-            Err(_)      => continue,
+            Err(_)      => DEFAULT_BREAK_MINS,
         };
     
 
@@ -54,11 +55,25 @@ fn main() {
     } // End loop.
 
     println!("Starting {}", task_name);
+    countdown(task_minutes);
+    println!("Time for a {} minute break!", break_minutes);
+    countdown(break_minutes);
+    println!("Break is over! Back to work!");
+}
 
-    let end_time = time::get_time() + Duration::seconds(60 * task_minutes);
-    while time::get_time() < end_time {
+
+fn print_time(duration: Duration) {
+    io::stdout().flush()
+        .expect("Could not flush stdout");
+    print!("\r{}:{}:{}   ", duration.num_hours(), duration.num_minutes() % 60, duration.num_seconds() % 60);
+}
+
+fn countdown(num_minutes: i64) {
+    let mut now  = time::now();
+    let end_time = now + Duration::minutes(num_minutes);
+    while now < end_time {
+        print_time(end_time - now);
         thread::sleep(std::time::Duration::from_secs(1));
-        println!("Time remaining: {}", end_time - time::get_time());
-    }
-
+        now = time::now();
+    } 
 }
